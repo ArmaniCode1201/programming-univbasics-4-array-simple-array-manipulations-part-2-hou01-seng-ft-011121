e5274f3c2cad7cb7f4a152be628867f51dbcfa01
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-26 19:31:17 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,8 @@
+def using_concat(my_favorite_things, all_my_favs)
+  my_favorite_things.concat(all_my_favs)
+end
+
+def using_insert(list_of_programming_languages, another_language)
+new_array = []
+new_array.insert(4, "Python")
+end
